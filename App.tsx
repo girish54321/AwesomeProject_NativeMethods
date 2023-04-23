319950780290e876c7bdf250c48f507d72e3e118
Girish Parate <girishparate@Girishs-iMac.local>
2023-04-23 18:59:01 +0530

    done

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-
 import React from 'react';
 import {
-  SafeAreaView, Text, NativeModules
+  SafeAreaView, Text, NativeModules, Platform
 } from 'react-native';
-
-
 
 function App(): JSX.Element {
 
   let buildInfo = NativeModules.ConfigModule.getConstants()
 
-  // console.log("Native", NativeModules.ConfigModule.getConstants());
-
   return (
     <SafeAreaView style={{ flex: 1, alignSelf: 'center', alignContent: 'center', justifyContent: 'center' }}>
-      <Text>iphone</Text>
+      <Text>{Platform.OS}</Text>
       <Text style={{ fontSize: 60 }}>{buildInfo.BUILD_ENV}</Text>
       <Text style={{ fontSize: 22 }}>{buildInfo.BASE_URL}</Text>
     </SafeAreaView>
